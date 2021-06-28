5c26a9793b3cc32c6b7a56621012686570cb6337
UTKARSH SRIVASTAVA <76895069+UtkarshSrivastava23@users.noreply.github.com>
2021-06-28 17:23:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var randomnumber1 = Math.floor(Math.random() * 6) + 1 ;
 
-var ranImg = "dice" + randomnumber1 + ".png";
-var ranSource = "images/" + ranImg;
+var ranImg1 = "dice" + randomnumber1 + ".png";
+
 
 var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src", ranSource);
+image1.setAttribute("src", ranImg1);
 
 
 var randomnumber2 = Math.floor(Math.random() * 6) + 1 ;
 
 var ranImg2 = "dice" + randomnumber2 + ".png";
-var ranSource2 = "images/" + ranImg2;
+
 
 var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src", ranSource2);
+image2.setAttribute("src", ranImg2);
 
 
 if (randomnumber1 > randomnumber2){
